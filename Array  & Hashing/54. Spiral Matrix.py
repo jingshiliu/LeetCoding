6776c56b69d4378044603b7c1c35aba9894a621b
Jingshi Liu <jingshiliu07@gmail.com>
2023-02-23 02:42:20 -0500

    did a slightly different solution for 54. Spiral Matrix

--- Array  & Hashing/54. Spiral Matrix.py
+++ Array  & Hashing/54. Spiral Matrix.py
@@ -48,6 +48,35 @@ def spiralOrder(matrix):
             curIndex = nextIndex
 
 
-matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+def spiralOrder2(matrix):
+    directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    curDirection = 0
+    isRow = True
+    cellsTotal = len(matrix[0]) * len(matrix)
+    visited = set()
+    res = []
+    curIndex = [0, 0]
+    while True:
+        if len(res) == cellsTotal: return res
+        num = matrix[curIndex[0]][curIndex[1]]
+        res.append(num)
+        visited.add(tuple(curIndex))
 
-print(spiralOrder(matrix))
+        direction = directions[curDirection]
+        nextIndex = [curIndex[0] + direction[0], curIndex[1] + direction[1]]
+        # move index
+        if ((nextIndex[1] == len(matrix[0]) or nextIndex[0] == len(matrix))
+                or (nextIndex[1] < 0 or nextIndex[0] < 0)
+                or tuple(nextIndex) in visited):
+            curDirection = (curDirection + 1) % 4
+            direction = directions[curDirection]
+            curIndex[0] += direction[0]
+            curIndex[1] += direction[1]
+        else:
+            curIndex = nextIndex
+
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+
+print(spiralOrder2(matrix))
