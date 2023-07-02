e308d055b89144c622cb97ee888fca88f629ef61
Jingshi Liu <jingshiliu07@gmail.com>
2023-07-02 17:23:41 -0400

    chore: add more efficient solution to 2352. Equal Row and Column Pairs

--- Array  & Hashing/2352. Equal Row and Column Pairs.py
+++ Array  & Hashing/2352. Equal Row and Column Pairs.py
@@ -1,4 +1,24 @@
-def equal_pairs(grid):
+from collections import defaultdict
+
+
+def equalPairs( grid) -> int:
+    # Time: O(n2)
+    # Space: O(n2)
+    N = len(grid)
+    row_pairs = defaultdict(lambda: 0)
+    res = 0
+
+    for r in range(N):
+        row_pairs[tuple(grid[r])] += 1
+
+    for c in range(N):
+        col_pair = tuple([grid[r][c] for r in range(N)])
+        res += row_pairs[col_pair]
+
+    return res
+
+
+def equal_pairs2(grid):
     COLS, ROWS = len(grid[0]), len(grid)
     row_nums = {}
     col_nums = {}
