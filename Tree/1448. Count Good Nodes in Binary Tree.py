1587a1212e6778eae031ce6382b0fb31c4f0d345
Jingshi Liu <jingshiliu07@gmail.com>
2023-07-09 14:43:45 -0400

    redo[Tree]: 1448. Count Good Nodes in Binary Tree

--- Tree/1448. Count Good Nodes in Binary Tree.py
+++ Tree/1448. Count Good Nodes in Binary Tree.py
@@ -4,23 +4,33 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def goodNodes(self, root: TreeNode) -> int:
-        res = 0
 
-        def findGood(node, maxNode):
-            if not node:
-                return
+def goodNodes(root) -> int:
+    def find_good(node, max_asc_val) -> int:
+        if node is None: return 0
+        cur_max_asc = max(node.val, max_asc_val)
 
-            if node.val >= maxNode:
-                nonlocal res
-                res += 1
-                findGood(node.left, node.val)
-                findGood(node.right, node.val)
-            else:
-                findGood(node.left, maxNode)
-                findGood(node.right, maxNode)
+        return int(cur_max_asc == node.val) + find_good(node.left, cur_max_asc) + find_good(node.right, cur_max_asc)
 
-        findGood(root, root.val)
-        return res
+    return find_good(root, root.val)
+
+
+def goodNodes1(root) -> int:
+    res = 0
+
+    def findGood(node, maxNode):
+        if not node:
+            return
+
+        if node.val >= maxNode:
+            nonlocal res
+            res += 1
+            findGood(node.left, node.val)
+            findGood(node.right, node.val)
+        else:
+            findGood(node.left, maxNode)
+            findGood(node.right, maxNode)
+
+    findGood(root, root.val)
+    return res
 
