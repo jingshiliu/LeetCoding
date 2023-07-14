174bc6440b8ad7886ac38cd6399f48d7d6d8df98
Jingshi Liu <jingshiliu07@gmail.com>
2023-07-14 00:21:55 -0400

    redo[Tree]: 236. Lowest Common Ancestor of a Binary Tree

--- Tree/236. Lowest Common Ancestor of a Binary Tree.py
+++ Tree/236. Lowest Common Ancestor of a Binary Tree.py
@@ -1,3 +1,33 @@
+def lowest_common_ancestor(root, p, q):
+    # CONDITION 1: self is p or q, if one of descendant is p or q, then it is the res
+    # running dfs
+    # is the first answer found the lowest?
+    # yes, bc if self is p or q and no descendant is the other one, meaning current branch has no other node
+    # and no node between the ANS and current node is the other vaonelue, bc that will satisfy the condition 1
+
+    # two situations: self is one node, and one descendant is other one
+    # find each one in each subtree (left and right)
+
+    res = None
+
+    def dfs(node):
+        nonlocal res
+        if not node or res is not None: return False
+
+        find_left = dfs(node.left)
+        find_right = dfs(node.right)
+
+        is_self = node == p or node == q
+        if (is_self and (find_left or find_right)) or (find_left and find_right):
+            res = node
+            return False
+
+        return is_self or find_left or find_right
+
+    dfs(root)
+    return res
+
+
 def lowestCommonAncestor(root, p, q):
     if not root: return None
 
