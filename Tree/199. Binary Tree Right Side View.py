a89791286a89faae8a20b5c5737dec421549cc1e
Jingshi Liu <jingshiliu07@gmail.com>
2023-07-14 00:47:11 -0400

    redo[Tree]: 199. Binary Tree Right Side View

--- Tree/199. Binary Tree Right Side View.py
+++ Tree/199. Binary Tree Right Side View.py
@@ -4,6 +4,26 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+def right_side_view(root) -> list[int]:
+    """
+    Time: O(n)
+    Space: O(n)
+    """
+    res = []
+
+    def dfs(node, depth):
+        if not node: return
+        if depth > len(res):
+            res.append(node.val)
+
+        dfs(node.right, depth + 1)
+        dfs(node.left, depth + 1)
+
+    dfs(root, 1)
+    return res
+
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         res = []
