2c24bf49ddba9db2119add1371069c7165274d72
Jingshi Liu <jingshiliu07@gmail.com>
2023-07-21 17:51:33 -0400

    redo[Heap/ Quickselect]: 215. Kth Largest Element in an Array

--- Heap or Priority Queue/215. Kth Largest Element in an Array.py
+++ Heap or Priority Queue/215. Kth Largest Element in an Array.py
@@ -1,3 +1,36 @@
+def find_kth_largest(nums:list[int], k: int) -> int:
+    # pivot
+    # performing quick select
+    # then swap pivot and left
+    # if pivot index is k, then return
+    # else do quick select again for either left or right side of pivot
+
+    # Time: O(n) average case N + N/2 + N/4 ... = 2N = O(n)
+    #       O(n2) worst case where pivot is always the smallest, we have to do quick select n times with each cost N
+    # Space: O(1) as we do in place swap
+
+    K_INDEX = len(nums) - k
+    L, R = 0, len(nums) - 1
+    while True:
+        pivot, l, r = L, L, R
+
+        while l < r:
+            while l < r and nums[r] >= nums[pivot]:
+                r -= 1
+            while l < r and nums[l] <= nums[pivot]:
+                l += 1
+
+            nums[l], nums[r] = nums[r], nums[l]
+
+        nums[l], nums[pivot] = nums[pivot], nums[l]
+        if l < K_INDEX:
+            L = l + 1
+        elif l > K_INDEX:
+            R = l - 1
+        else:
+            return nums[l]
+
+
 def findKthLargest(nums: list[int], k: int) -> int:
     k = len(nums) - k
     l, r = 0, len(nums) - 1
